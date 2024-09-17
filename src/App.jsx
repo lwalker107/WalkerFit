0376ddcdf487aa182094505f755d278dd11495f8
lwalker <landen@example.com>
2024-09-17 15:13:35 -0500

    layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,36 @@
+const App = () => (
+    <main className='relative'> 
+      nav
+      <section className='xl:padding-1 wide:padding-r 
+      padding-b'>
+        Hero
+      </section>
+      <section className='padding'>
+        Features
+      </section>
+      <section className='padding'>
+        Create custom workouts
+      </section>
+      <section className='padding'>
+        Follow workout plans
+      </section>
+      <section className='padding'>
+        Community
+      </section>
+      <section className='padding'>
+        Follow favorite fitness influencers
+      </section>
+      <section className='padding'>
+        Tracking stats
+      </section>
+      <section className='padding'>
+        Challenges
+      </section>
+      <section className='padding'>
+        Achievements
+      </section>
+    </main>
+    
+)
 
-function App() {
-
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
-
-export default App
+export default App;
