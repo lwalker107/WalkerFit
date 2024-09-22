974804968525857caee0dfc0a7086a04cde190e1
lwalker <landen@example.com>
2024-09-22 12:42:43 -0500

    general button look but need to change arrow

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import Button from '../components/Button'
+import { arrowRight } from '../assets/assets/icons'
 
 const Hero = () => {
   return (
@@ -9,6 +11,18 @@ const Hero = () => {
       justify-center min-h-screen 
       gap-10 max-container'
     >
+      <div className='relative xl:w-2/5 flex flex-col 
+      justify-center items-start w-full max-xl:padding-x pt-28'>
+        <h1>
+          <span>Let YOUR fitness journey</span>
+          <br />
+          <span>begin anywhere!</span>
+          <p>
+            See how far you can get today and sign up!
+          </p>
+          <Button label="Download now" iconURL={arrowRight}/>
+        </h1>
+      </div>
 
     </section>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -23,7 +23,7 @@ export default {
       },
       colors: {
         'primary': "#ECEEFF",
-        "coral-red": "#FF6452",
+        "teal": "#38B19E",
         "slate-gray": "#6D6D6D",
         "pale-blue": "#F5F6FF",
         "white-400": "rgba(255, 255, 255, 0.80)"
